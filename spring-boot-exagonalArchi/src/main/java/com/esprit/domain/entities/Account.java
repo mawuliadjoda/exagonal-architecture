6c7403968220ce47e6e9fd00a761a645b1a6892a
mawuli <koffimawuli.adjoda@gmail.com>
2021-12-01 11:55:33 +0100

    add lombock

--- spring-boot-exagonalArchi/src/main/java/com/esprit/domain/entities/Account.java
+++ spring-boot-exagonalArchi/src/main/java/com/esprit/domain/entities/Account.java
@@ -2,34 +2,11 @@ package com.esprit.domain.entities;
 
 import java.time.LocalDate;
 
+import lombok.Data;
+
+@Data
 public class Account {
-
-	private Long accountNumber;
-	private Double balance;
-	private LocalDate date;
-
-	public Long getAccountNumber() {
-		return accountNumber;
-	}
-
-	public void setAccountNumber(Long accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-
-	public Double getBalance() {
-		return balance;
-	}
-
-	public void setBalance(Double balance) {
-		this.balance = balance;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-
+	Long accountNumber;
+	Double balance;
+	LocalDate date;
 }
--- spring-boot-exagonalArchi/src/main/java/com/esprit/infrastructure/database/AccountJpa.java
+++ spring-boot-exagonalArchi/src/main/java/com/esprit/infrastructure/database/AccountJpa.java
@@ -8,36 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "account")
+@Data
 public class AccountJpa {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long accountNumber;
-	private Double balance;
-	private LocalDate date;
-
-	public Long getAccountNumber() {
-		return accountNumber;
-	}
-
-	public void setAccountNumber(Long accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-
-	public Double getBalance() {
-		return balance;
-	}
-
-	public void setBalance(Double balance) {
-		this.balance = balance;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long accountNumber;
+	Double balance;
+	LocalDate date;
 }
